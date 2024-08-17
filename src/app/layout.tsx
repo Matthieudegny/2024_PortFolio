8247595a74476332358e053d7309067335d856f7
Matthieu <matthieu.degny.dev@gmail.com>
2024-08-17 17:27:03 +0200

    change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Software Developer",
-  description: "Front end",
+  title: "Matthieu DEGNY Portfolio",
+  description: "Software Developer Portfolio",
 };
 
 export default function RootLayout({
@@ -14,6 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
+      <Head>
+        <title>{String(metadata.title) ?? "Default Title"}</title>
+        <meta
+          name="description"
+          content={String(metadata.description) ?? "Default Description"}
+        />
+      </Head>
       <body>{children}</body>
     </html>
   );
